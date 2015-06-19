d3ed039cc8a44cb2adc39cef1945bba4b02efd80
Jonathan Sheely <jSheely@twentytech.net>
2015-06-18 20:59:04 -0400

    Update npm.cmd

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -1,6 +1,2 @@
 :: Created by npm, please don't edit manually.
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
-) ELSE (
-  node npm-cli.js
-)
+node npm-cli.js
